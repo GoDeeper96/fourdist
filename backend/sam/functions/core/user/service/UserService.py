ab8fa10af625b150f0bc450b6efe3c6acb5d19dc
sebastian-julon <u201522391@upc.edu.pe>
2026-01-09 00:15:23 -0500

    ecoroco

--- backend/sam/functions/core/user/service/UserService.py
+++ backend/sam/functions/core/user/service/UserService.py
@@ -71,3 +71,17 @@ class UserService():
                 
             except Exception as e:
                 logger.error(f"Error: {e}")
+    def get_healthv5(self, item: Dict[str, Any] = None) -> Tuple[int, Dict[str, Any]]:
+            """Get area by ID"""
+            try:
+                
+                test = "test"
+                
+                return 200, {
+                    "message": "Ok",
+                    "data": test
+                }
+                
+                
+            except Exception as e:
+                logger.error(f"Error: {e}")
